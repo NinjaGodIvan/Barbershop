7d480f72f23a0928a0a113beb4e8f583c9e98d9b
yggy123 <gyasimose@gmail.com>
2019-07-18 20:10:23 -0700

    Add files via upload

---
+++ WaitList.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+// currently configured to use customer Id as a parameter, can be switched out when customer ID class is built
+class WaitList<CustomerId> {
+	private static String barberShopId = null;
+	private ArrayList<CustomerId> list;
+	
+	public WaitList(String shopId) {
+		barberShopId = shopId;
+	}
+	public void addToWait(CustomerId c) {
+		list.add(c);
+	}
+	public String getCurrentWaitList() {
+		String currentWaitList = "";
+		for(int i = 0; i < list.size(); i++) {
+			currentWaitList += (list.get(i).toString() + "\n");
+		}
+		return currentWaitList;
+	}
+	public String estimateWait() {
+		double waitTime = (list.size()*30)/60;
+		return "Wait time is " + "%.1f" + waitTime + "hrs";
+	}
+	public CustomerId waitListPop() {
+		CustomerId nextCustomer = list.get(0); //get first customer on list, remove if non responsive.
+		list.remove(0); // clear first customer from queue
+		String customerContact = nextCustomer.getPhoneNumber();// get customer phone number and notify
+		// notify customer of appointment	
+		return nextCustomer; // return customer ID to the requesting barbershop
+	}	
+}
