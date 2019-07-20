0398d14b3cc5ce32453c2d0f7c7c2ff43dfd302c
yggy123 <gyasimose@gmail.com>
2019-07-20 14:56:15 -0700

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -4,9 +4,12 @@ package barber;
 import java.util.Calendar;
 
 public class Appointment {
+	Calendar date;
+	CustomerInfo customer;
+	BarberInfo barber;
 		
 	/** Function that adds a customer's appointment */
-	public void makeAppointment(Calendar appointment, int day, int month, int year, int min, int hour) {
+	public void makeAppointment(BarberInfo barber, CustomerInfo customer, Calendar appointment, int day, int month, int year, int min, int hour) {
 		
 		//Sets the customer's appointment based on the input
 		appointment = Calendar.getInstance();
@@ -42,22 +45,27 @@ public class Appointment {
 		else {
 			//Goes to the next month for the correct appointment's month
 			appointment.add(Calendar.MONTH, 1);
+			this.date = appointment; // assign appointment to the date of appointment
+			this.customer = customer; // assigning customer to appointment
+			this.barber = barber; // assigning barber to appointment
+			this.barber.appointments.add(this); // assigning appointment to barber
+			this.customer.custAppointment = this; // assigning appointment to customer
 			System.out.println("Success! Your appointment had been added!");
 		}
 	}
 	
 	/** Function that gets the customer's appointment */
-	public String getAppointment(Calendar cal) {
+	public String getAppointment() {
 		
 		int day, monthInInt, year, min, hour;
 		String month = "January";
 		String [] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 		
-		day = cal.get(Calendar.DATE);
-		monthInInt = cal.get(Calendar.MONTH) + 1;
-		year = cal.get(Calendar.YEAR);
-		min = cal.get(Calendar.MINUTE);
-		hour = cal.get(Calendar.HOUR);	
+		day = this.date.get(Calendar.DATE);
+		monthInInt = this.date.get(Calendar.MONTH) + 1;
+		year = this.date.get(Calendar.YEAR);
+		min = this.date.get(Calendar.MINUTE);
+		hour = this.date.get(Calendar.HOUR);	
 		
 		for(int a = 0; a < months.length; a++)
 			if(a == (monthInInt - 1)) {
@@ -77,5 +85,9 @@ public class Appointment {
 	public void cancelAppointment() {
 		
 		//Code this
+	}
+	@Override
+	public String toString() {
+		return "Appointment [date=" + date + ", customer=" + customer + ", barber=" + barber + "]";
 	}
 }
