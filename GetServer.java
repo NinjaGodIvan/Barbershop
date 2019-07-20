21452e6dc4a5889b32fc863f383926c538600738
VladislavRusk <49698202+VladislavRusk@users.noreply.github.com>
2019-07-20 16:01:24 -0700

    Update GetServer.java

--- GetServer.java
+++ GetServer.java
@@ -16,6 +16,98 @@ public class GetServer {
 	 */
 	GetServer() {
 	}
+// CUSTOMER INTERFACE CLASSES YOU CAN USE	
+	
+	public giveCustomerInfo(CustomerInfo info) {
+		this.sendInfo(new Packet(
+	}
+	
+	/*
+		@return will return customerinfo class if found and null if no account
+	*/	
+	
+	public CustomerInfo getCustomerInfo(String userName) {
+		Packet packet = new Packet((new CustomerInfo(userName)),RequestEnum.request.getData);
+		packet = this.getInfo(packet);
+		return packet.getCustomer();
+	}
+	
+	/*
+		@return will return customerinfo class if found and null if no account
+		(COMPARES PASSWORD)
+	*/
+	
+	public CustomerInfo getCustomerInfo(String userName, String password) {
+		return this.findCustomerInfo(username,password);
+	}	
+	
+	public CustomerInfo getCustomerInfo(CustomerInfo info) {
+		Packet packet = new Packet(info,RequestEnum.request.getData);
+		packet = this.getInfo(packet);
+		return packet.getCustomer();
+	}	
+	
+	/*
+		@return returns true if created account false if account already exists
+	*/
+	public boolean createCustomerInfoAccount(CustomerInfo info) {
+		if (this.findCustomerInfo(info.getUserName(),info.getPassword()) == null) {
+			this.sendInfo(new Packet(info,RequestEnum.Request.giveData));
+			return true;
+		} else {
+			System.out.println("ACCOUNT EXISTS");
+		}		
+		return false;
+	}	
+	
+// BARBERSHOP INTERFACE CLASSES
+	public BarberShopInfo getBarberShopInfo(String userName) {
+		Packet packet = new Packet((new BarberShopInfo(userName)),RequestEnum.request.getData);
+		packet = this.getInfo(packet);
+		return packet.getBarberShop();
+	}	
+	
+	/*
+		@return returns account if it found it based on username or password
+		Will Return null if it doesnt find account
+	*/
+	public BarberShopInfo getBarberShopInfo(String userName, String password) {
+		return this.findBarberShopInfo(username,password);
+	}	
+	
+	public BarberShopInfo getBarberShopInfo(BarberShopInfo info) {
+		Packet packet = new Packet(info,RequestEnum.request.getData);
+		packet = this.getInfo(packet);
+		return packet.getBarberShop();
+	}
+	
+	
+	/*
+		@return returns true if created account false if account already exists
+	*/	
+	public boolean createBarberShopAccount(BarberShopInfo info) {
+		if (this.findBarberShopInfo(info.getUserName(),info.getPassword()) == null) {
+			this.sendInfo(new Packet(info,RequestEnum.Request.giveData));
+			return true;
+		} else {
+			System.out.println("ACCOUNT EXISTS");
+		}		
+		return false;
+	}	
+	
+	
+
+// DONT WORRY ABOUT THESE CLASSES
+
+	private CustomerInfo findCustomerInfo(String userName, String password) {
+		Packet packet = new Packet((new CustomerInfo(userName,password)),RequestEnum.request.findData);
+		return packet.getCustomer();
+	}	
+	
+	private BarberShopInfo findBarberShopInfo(String userName, String password) {
+		Packet packet = new Packet((new CustomerInfo(userName,password)),RequestEnum.request.findData);
+		return packet.getCustomer();
+	}
 	
 	/* returns true of connected returns false otherwise
 	 * 
@@ -32,7 +124,7 @@ public class GetServer {
 		return null;
 	}
 	
-	public void sendInfo(Packet info) {
+	private void sendInfo(Packet info) {
 		try {
 			Socket socket = this.connectToServer();
 			
@@ -46,7 +138,7 @@ public class GetServer {
 		}
 	}
 		
-	public Packet getInfo(Packet info) {
+	private Packet getInfo(Packet info) {
 		Packet packet = null;
 		try {
 			this.sendInfo(info);
