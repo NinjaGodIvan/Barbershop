93f3d5d5ae0a5c4ce71191f3e2ca302c5339207f
yggy123 <gyasimose@gmail.com>
2019-07-21 14:59:01 -0700

    Update BarbershopInfo.java

--- BarbershopInfo.java
+++ BarbershopInfo.java
@@ -16,40 +16,46 @@ public class BarberShopInfo implements Serializable {
 		this.password = password;
 		this.address = "nothing for now";
 	}
-	
-	BarberShopInfo(String userName) { // just a test constructor
-		this("test", userName,"test",5555,"no password");
-	}
-
-	BarberShopInfo(String userName,String password) { // just a test constructor
-		this("test", userName,"test",5555,password);
-	}
-	
 	public String getAddress() {
 		return address;
 	}
-	
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	public long getPhone() {
 		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
 	}
 	
 	public String getName() {
 		return name;
 	}	
-	
+	public void setName(String name) {
+		this.name = name;
+	}
 	public String getUserName() {
 		return userName;
 	}
-	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 	public String getEmail() {
 		return email;
 	}
-	
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	public String getPassword() {
 		return password;
 	}
-	
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	public String toString() {
-		return "Name" + userName;
+		return "BarbershopInfo [name=" + name + ", email=" + email
+				+ ", userName=" + userName + ", phone=" + phone + ", address="
+				+ address + "]";
 	}
 }
