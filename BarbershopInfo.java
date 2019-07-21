cd9033b84e5009f5563ae137f8f3c1cce335e00a
Chaobing Wang <43365553+Chaobinglaw@users.noreply.github.com>
2019-07-21 15:04:59 -0700

    Update BarbershopInfo.java

--- BarbershopInfo.java
+++ BarbershopInfo.java
@@ -7,7 +7,6 @@ public class BarberShopInfo implements Serializable {
 	private long phone;
 	private String password;
 	private String address;
-	private Waitlist wList;
 	
 	BarberShopInfo(String name,String userName,String email,long phone,String password) {
 		this.name = name;
@@ -17,46 +16,40 @@ public class BarberShopInfo implements Serializable {
 		this.password = password;
 		this.address = "nothing for now";
 	}
+	
+	BarberShopInfo(String userName) { // just a test constructor
+		this("test", userName,"test",5555,"no password");
+	}
+
+	BarberShopInfo(String userName,String password) { // just a test constructor
+		this("test", userName,"test",5555,password);
+	}
+	
 	public String getAddress() {
 		return address;
 	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
+	
 	public long getPhone() {
 		return phone;
-	}
-	public void setPhone(long phone) {
-		this.phone = phone;
 	}
 	
 	public String getName() {
 		return name;
 	}	
-	public void setName(String name) {
-		this.name = name;
-	}
+	
 	public String getUserName() {
 		return userName;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+	
 	public String getEmail() {
 		return email;
 	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	
 	public String getPassword() {
 		return password;
 	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	
 	public String toString() {
-		return "BarbershopInfo [name=" + name + ", email=" + email
-				+ ", userName=" + userName + ", phone=" + phone + ", address="
-				+ address + "]";
+		return "Name" + userName;
 	}
 }
