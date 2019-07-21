0bf0d71862fad3223fb0b432cd028f695f4ebbcf
yggy123 <gyasimose@gmail.com>
2019-07-21 15:06:42 -0700

    Update BarbershopInfo.java

--- BarbershopInfo.java
+++ BarbershopInfo.java
@@ -28,27 +28,40 @@ public class BarberShopInfo implements Serializable {
 	public String getAddress() {
 		return address;
 	}
-	
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	public long getPhone() {
 		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
 	}
 	
 	public String getName() {
 		return name;
 	}	
-	
+	public void setName(String name) {
+		this.name = name;
+	}
 	public String getUserName() {
 		return userName;
 	}
-	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 	public String getEmail() {
 		return email;
 	}
-	
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	public String getPassword() {
 		return password;
 	}
-	
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	public String toString() {
 		return "Name" + userName;
 	}
