31b3c31846c412b7b597065005a7c994e9d5d920
Chaobing Wang <43365553+Chaobinglaw@users.noreply.github.com>
2019-07-21 14:09:08 -0700

    Update BarbershopInfo.java

--- BarbershopInfo.java
+++ BarbershopInfo.java
@@ -1,75 +1,55 @@
-package userInfo;
-
 import java.io.Serializable;
 
-public class BarbershopInfo implements Serializable{
+public class BarberShopInfo implements Serializable {
+	private String name;
+	private String email;
 	private String userName;
-	private int barbershopId;
-	private String barbershopName;
-	private String barbershopEmail;
-	private int barbershopPhone;
+	private long phone;
+	private String password;
+	private String address;
 	
-	
-
-	public BarbershopInfo(String userName, int barbershopId, String barbershopName, String barbershopEmail,
-			int barbershopPhone) {
-		super();
+	BarberShopInfo(String name,String userName,String email,long phone,String password) {
+		this.name = name;
 		this.userName = userName;
-		this.barbershopId = barbershopId;
-		this.barbershopName = barbershopName;
-		this.barbershopEmail = barbershopEmail;
-		this.barbershopPhone = barbershopPhone;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+		this.address = "nothing for now";
+	}
+	
+	BarberShopInfo(String userName) { // just a test constructor
+		this("test", userName,"test",5555,"no password");
 	}
 
+	BarberShopInfo(String userName,String password) { // just a test constructor
+		this("test", userName,"test",5555,password);
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public long getPhone() {
+		return phone;
+	}
+	
+	public String getName() {
+		return name;
+	}	
+	
 	public String getUserName() {
 		return userName;
 	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getBarbershopName() {
-		return barbershopName;
-	}
-
-	public void setBarbershopName(String barbershopName) {
-		this.barbershopName = barbershopName;
-	}
-
-	public String getBarbershopEmail() {
-		return barbershopEmail;
-	}
-
-	public void setBarbershopEmail(String barbershopEmail) {
-		this.barbershopEmail = barbershopEmail;
-	}
-
-	public int getBarbershopPhone() {
-		return barbershopPhone;
-	}
-
-	public void setBarbershopPhone(int barbershopPhone) {
-		this.barbershopPhone = barbershopPhone;
-	}
-
 	
-
-	public int getBarbershopId() {
-		return barbershopId;
+	public String getEmail() {
+		return email;
 	}
-
-	public void setBarbershopId(int barbershopId) {
-		this.barbershopId = barbershopId;
+	
+	public String getPassword() {
+		return password;
 	}
-
-	@Override
+	
 	public String toString() {
-		return "BarbershopInfo [userName=" + userName + ", barbershopId=" + barbershopId + ", barbershopName="
-				+ barbershopName + ", barbershopEmail=" + barbershopEmail + ", barbershopPhone=" + barbershopPhone
-				+ "]";
+		return "Name" + userName;
 	}
-	
-	
-	
 }
