22a1d18383aafe0c9d7d7f580f2dfdeae0d16e45
yggy123 <gyasimose@gmail.com>
2019-07-28 16:54:50 -0700

    Update BarbershopMainUI.java

--- BarbershopMainUI.java
+++ BarbershopMainUI.java
@@ -5,7 +5,9 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
-
+/*
+*	All the tools I used, and then some... kept having random issues so just pulled everything in, explicitly and implicitly.
+*/
 import java.awt.*;
 import javax.swing.*;
 
@@ -19,7 +21,7 @@ public class BarbershopMainUI extends javax.swing.JFrame {
 	static BarberShop barberShop;
 	static CustomerInfo customerOrBarber;
 	static Tree infoTree;
-	
+	//  All of the buttons and panels, plus some extras that may have gotten overlooked and under utilized.
 	javax.swing.JButton barberButton, backButton, barberMakeButton, barberAddButton, customerAppointmentsButton, customerWaitlistButton,
 				signoutButton, customerMenuButton, addButton, makeButton;
 	javax.swing.JPanel buttonsPanel, bottomPanel, mainPanel, customerMenuPanel, addBarberPanel, showAppointmentPanel,
@@ -27,23 +29,19 @@ public class BarbershopMainUI extends javax.swing.JFrame {
 	javax.swing.JTextArea dataReturn;
 	javax.swing.JLabel nameLabel;
     javax.swing.JTextField userText;
-    java.awt.GridBagConstraints pos;
+    java.awt.GridBagConstraints pos;  
 
-    
-
-public JPanel barbershopMainPanel() {
-		
+public JPanel barbershopMainPanel() { // The main entry point for the Barbershop User
 		mainPanel = new JPanel();
 				
 		nameLabel = new JLabel(barberShop.shopInfo.getUserName()); // name of barbershop
 		nameLabel.setFont(new Font("Tahoma",0 , 15));
 		mainPanel.add(nameLabel);
-
+		// build panel for Button placement
 		buttonsPanel = new JPanel(new GridBagLayout());
 		barberButton = new JButton("Manage Barbers");
 		barberButton.addActionListener(new ActionListener() {
-			
-			//This will remove the current panel being displayed and create a panel to be displayed on the frame
+			// switch panels to Barber Management
 			public void actionPerformed(ActionEvent e) {			
 				mainPanel.removeAll();
 				JPanel panel = ManageBarberPanel();
@@ -51,13 +49,11 @@ public JPanel barbershopMainPanel() {
 				panel.repaint();
 				panel.revalidate();
 			}
-		});
-		
+		});		
 		customerMenuButton = new JButton("Manage Customers");
-		customerMenuButton.addActionListener(new ActionListener() {
-			
+		customerMenuButton.addActionListener(new ActionListener() {			
 			public void actionPerformed(ActionEvent e) {
-				
+			// Switch panels to Customer Management  -- includes View waitlist and view appointments. 
 				mainPanel.removeAll();
 				JPanel panel = CustomerMenuPanel();
 				add(panel);
@@ -65,32 +61,24 @@ public JPanel barbershopMainPanel() {
 				panel.revalidate();
 			}
 		});
-			
-		//Used to position buttons
+		// Button Placement
 		pos = new GridBagConstraints();
-		pos.insets = new Insets(40,40,40,40);
-		
+		pos.insets = new Insets(40,40,40,40);		
 		buttonsPanel.add(customerMenuButton, pos);
 		buttonsPanel.add(barberButton, pos);
-
+		// place buttons on panel
 		mainPanel.add(buttonsPanel);
-
 		return mainPanel;
 	}
                                             
 	public JPanel CustomerMenuPanel() {
 		customerMenuPanel = new JPanel();
-		/*
-		 * 	nameLabel = new JLabel(barberShop.shopInfo.getUserName()); // name of barbershop
-		 *	nameLabel.setFont(new Font("Tahoma",0 , 15));
-		 * 
-		 */		
 		customerMenuPanel.add(nameLabel);
+		// Build panel for buttons placement
 		buttonsPanel = new JPanel(new GridBagLayout());
 		customerAppointmentsButton = new JButton("show today's Appointments"); // Display the day's appointments
 		customerAppointmentsButton.addActionListener(new ActionListener() {
-			
-			//This will remove the current panel being displayed and create a panel to be displayed on the frame
+			// Go to show the appointments panel
 			public void actionPerformed(ActionEvent e) {			
 				customerMenuPanel.removeAll();
 				JPanel panel = showAppointmentPanel();
@@ -98,10 +86,10 @@ public JPanel barbershopMainPanel() {
 				panel.repaint();
 				panel.revalidate();
 			}	
-		});
-		
+		});	
 		customerWaitlistButton = new JButton("Show current Waitlist"); // Display the waitlist contents
 		customerWaitlistButton.addActionListener(new ActionListener() {
+			// go to show the waitlist panel
 			public void actionPerformed(ActionEvent e) {
 				customerMenuPanel.removeAll();
 				JPanel panel = showWaitlistPanel();
@@ -112,15 +100,13 @@ public JPanel barbershopMainPanel() {
 		});
 		backButton = new JButton("Back");
 		backButton.addActionListener(new ActionListener() {
-			
-			public void actionPerformed(ActionEvent e) {
-				
+			// go to back to the main panel
+			public void actionPerformed(ActionEvent e) {		
 				customerMenuPanel.removeAll();
 				JPanel panel = barbershopMainPanel();
 				add(panel);
 				panel.repaint();
 				panel.revalidate();
-
 			}
 		});
 		
@@ -136,20 +122,17 @@ public JPanel barbershopMainPanel() {
 	public JPanel showWaitlistPanel() {
 		showWaitlistPanel = new JPanel();
 		dataReturn = new JTextArea();	
-		dataReturn.setText(barberShop.shopInfo.currentList.getCurrentWaitlist());
-		
+		//	Build the string of the current waitlist
+		dataReturn.setText(barberShop.shopInfo.currentList.getCurrentWaitlist());	
 		buttonsPanel = new JPanel(new GridBagLayout());
-		backButton = new JButton("Back");
-		backButton.addActionListener(new ActionListener() {
-			
+		backButton = new JButton("Back");	// go to back to the main panel
+		backButton.addActionListener(new ActionListener() {			
 			public void actionPerformed(ActionEvent e) {
-				
 				showWaitlistPanel.removeAll();
 				JPanel panel = CustomerMenuPanel();
 				add(panel);
 				panel.repaint();
 				panel.revalidate();
-
 			}
 		});
 		pos = new GridBagConstraints();
@@ -160,39 +143,35 @@ public JPanel barbershopMainPanel() {
 		
 		return showWaitlistPanel;
 	}
-
 	public JPanel showAppointmentPanel() {
 		showAppointmentPanel = new JPanel();
-		dataReturn = new JTextArea();	
-		//dataReturn.setText(barberShop.shopInfo.getAppointments());
-		dataReturn.setText("Apointments Sseete");
+		dataReturn = new JTextArea();	// will display the appointments on the calendar, in a list
+		dataReturn.setText(barberShop.shopInfo.getAppointments());
 		buttonsPanel = new JPanel(new GridBagLayout());
 		backButton = new JButton("Back");
-		backButton.addActionListener(new ActionListener() {
-			
+		backButton.addActionListener(new ActionListener() {		
 			public void actionPerformed(ActionEvent e) {
-				
 				showAppointmentPanel.removeAll();
 				JPanel panel = CustomerMenuPanel();
 				add(panel);
 				panel.repaint();
 				panel.revalidate();
-
 			}
 		});
 		pos = new GridBagConstraints();
 		pos.insets = new Insets(40,40,40,40);	
 		buttonsPanel.add(backButton, pos);
 		showAppointmentPanel.add(buttonsPanel);
-		showAppointmentPanel.add(dataReturn);  // adding string current Appointments
-		
+		showAppointmentPanel.add(dataReturn);  // adding string current Appointments	
 		return showAppointmentPanel;
 	}
-
+	/*	Now to manage the barbers -- includes only Add a barber to staff AND
+	*	Make a new barber, 
+	*	-- Then add the new barber to staff
+	*/
 	public JPanel ManageBarberPanel() {
 		manageBarberPanel = new JPanel();
-		manageBarberPanel.add(nameLabel);
-		
+		manageBarberPanel.add(nameLabel);	
 		buttonsPanel = new JPanel(new GridBagLayout());
 		barberMakeButton = new JButton("Make a Barber and Add to Staff"); 
 		barberMakeButton.addActionListener(new ActionListener() {
@@ -204,8 +183,7 @@ public JPanel barbershopMainPanel() {
 				panel.repaint();
 				panel.revalidate();
 			}	
-		});
-		
+		});	
 		barberAddButton = new JButton("Add a Barber to Staff"); 
 		barberAddButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -216,20 +194,18 @@ public JPanel barbershopMainPanel() {
 				panel.revalidate();
 			}
 		});
-		backButton = new JButton("Back");
+		backButton = new JButton("Back");  // go back to main Barber menu panel
 		backButton.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent e) {
 				
 				manageBarberPanel.removeAll();
-				JPanel panel = CustomerMenuPanel();
+				JPanel panel = ManageBarberPanel();
 				add(panel);
 				panel.repaint();
 				panel.revalidate();
-
 			}
 		});
-		
 		pos = new GridBagConstraints();
 		pos.insets = new Insets(40,40,40,40);	
 		buttonsPanel.add(barberMakeButton, pos);
@@ -238,7 +214,6 @@ public JPanel barbershopMainPanel() {
 		customerMenuPanel.add(buttonsPanel);	
 		return manageBarberPanel;
 	}
-
 	protected JPanel AddBarberPanel() {
 		addBarberPanel = new JPanel();
 		addBarberPanel.add(nameLabel);
